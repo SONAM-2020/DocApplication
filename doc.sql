157052b218587626d9d26a9418d81e0276473f79
BHUTANSYNC INFOTECH <sdorji815@gmail.com>
2021-12-16 16:51:04 +0600

    db changes

---
+++ doc.sql
@@ -0,0 +1,45 @@
+/*
+SQLyog Ultimate v8.82 
+MySQL - 5.7.34-log : Database - docapplication_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`docapplication_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `docapplication_db`;
+
+/*Table structure for table `tbl_users` */
+
+DROP TABLE IF EXISTS `tbl_users`;
+
+CREATE TABLE `tbl_users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `Phone` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Role_Id` int(11) DEFAULT NULL,
+  `Status` enum('Active','Inactive') DEFAULT NULL,
+  `Created_Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_users` */
+
+LOCK TABLES `tbl_users` WRITE;
+
+insert  into `tbl_users`(`Id`,`Name`,`Phone`,`Password`,`Role_Id`,`Status`,`Created_Date`) values (2,'Sonam','12345678','123',2,'Active','2021-12-15 16:24:20'),(3,'Sonam','17458669','$2y$10$dZIdHU3/LebzL/FIItKAWeJ8hPJaV.DIp9VynMS8lzP.UIxoZ3Yk.',2,'Active','2021-12-16 11:16:57');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
